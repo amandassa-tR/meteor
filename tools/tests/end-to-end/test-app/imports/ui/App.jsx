f5fa49e142c4c0b98c16b7984f495222336896e0
luccarojasc <59325078+luccarojasc@users.noreply.github.com>
2025-07-17 16:27:14 -0300

    End to end testing (#3)
    * Add end-to-end testing infrastructure
    
    * collections + forms and events
    
    remove + first styling
    
    filter tasks
    
    ---------
    
    Co-authored-by: Amanda Silva <amanda.silva@testrigor.com>

--- tools/tests/end-to-end/test-app/imports/ui/App.jsx
+++ tools/tests/end-to-end/test-app/imports/ui/App.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React, { useState } from 'react';
+import { useTracker, useSubscribe } from "meteor/react-meteor-data";
+import { TasksCollection } from "/imports/api/TasksCollection";
+import { Task } from "./Task";
+import { TaskForm } from "./TaskForm";
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+export const App = () => {
+  const [hideCompleted, setHideCompleted] = useState(false);
+   const hideCompletedFilter = { isChecked: { $ne: true } };
+  const handleToggleChecked = ({ _id, isChecked }) =>
+    Meteor.callAsync("tasks.toggleChecked", { _id, isChecked });
+  const isLoading = useSubscribe("tasks");
+  const tasks = useTracker(() =>
+    TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 },
+    }).fetch()
+  );
+  const handleDelete = ({ _id }) =>
+    Meteor.callAsync("tasks.delete", { _id });
+   const pendingTasksCount = useTracker(() =>
+    TasksCollection.find(hideCompletedFilter).count()
+  );
+  const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
+
+  if (isLoading()) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>Meteor with testRigor {pendingTasksTitle}</h1>
+          </div>
+        </div>
+      </header>
+      <div className="main">
+        <TaskForm />
+        <div className="filter">
+          <button onClick={() => setHideCompleted(!hideCompleted)}>
+            {hideCompleted ? 'Show All' : 'Hide Completed'}
+          </button>
+        </div>
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={handleToggleChecked}
+              onDeleteClick={handleDelete}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
