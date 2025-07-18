f54d815a65305e18bc29388b695d4e95c4b83a40
luccarojasc <59325078+luccarojasc@users.noreply.github.com>
2025-07-18 13:13:55 -0300

    End to end testing (#4)
    * Add end-to-end testing infrastructure
    
    * collections + forms and events
    
    remove + first styling
    
    filter tasks
    
    * login and other updates
    
    ---------
    
    Co-authored-by: Amanda Silva <amanda.silva@testrigor.com>

--- tools/tests/end-to-end/test-app/imports/ui/App.jsx
+++ tools/tests/end-to-end/test-app/imports/ui/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { useTracker, useSubscribe } from "meteor/react-meteor-data";
 import { TasksCollection } from "/imports/api/TasksCollection";
 import { Task } from "./Task";
 import { TaskForm } from "./TaskForm";
+import { LoginForm } from './LoginForm';
 
 export const App = () => {
   const [hideCompleted, setHideCompleted] = useState(false);
@@ -37,22 +38,33 @@ export const App = () => {
         </div>
       </header>
       <div className="main">
-        <TaskForm />
-        <div className="filter">
-          <button onClick={() => setHideCompleted(!hideCompleted)}>
-            {hideCompleted ? 'Show All' : 'Hide Completed'}
-          </button>
-        </div>
-        <ul className="tasks">
-          {tasks.map((task) => (
-            <Task
-              key={task._id}
-              task={task}
-              onCheckboxClick={handleToggleChecked}
-              onDeleteClick={handleDelete}
-            />
-          ))}
-        </ul>
+        {user ? (
+          <Fragment>
+            <div className="user" onClick={logout}>
+              {user.username} 🚪
+            </div>
+            <TaskForm />
+
+            <div className="filter">
+              <button onClick={() => setHideCompleted(!hideCompleted)}>
+                {hideCompleted ? 'Show All' : 'Hide Completed'}
+              </button>
+            </div>
+
+            <ul className="tasks">
+              {tasks.map(task => (
+                <Task
+                  key={task._id}
+                  task={task}
+                  onCheckboxClick={handleToggleChecked}
+                  onDeleteClick={handleDelete}
+                />
+              ))}
+            </ul>
+          </Fragment>
+        ) : (
+          <LoginForm />
+        )}
       </div>
     </div>
   );
