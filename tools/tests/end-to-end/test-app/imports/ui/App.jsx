9e728b8de82b23d1551c1134e2862528433cd8d0
amandassa-tR <amanda.silva@testrigor.com>
2025-07-18 16:34:36 +0000

    Revert "End to end testing (#4)"
    This reverts commit f54d815a65305e18bc29388b695d4e95c4b83a40.

--- tools/tests/end-to-end/test-app/imports/ui/App.jsx
+++ tools/tests/end-to-end/test-app/imports/ui/App.jsx
@@ -1,9 +1,8 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState } from 'react';
 import { useTracker, useSubscribe } from "meteor/react-meteor-data";
 import { TasksCollection } from "/imports/api/TasksCollection";
 import { Task } from "./Task";
 import { TaskForm } from "./TaskForm";
-import { LoginForm } from './LoginForm';
 
 export const App = () => {
   const [hideCompleted, setHideCompleted] = useState(false);
@@ -38,33 +37,22 @@ export const App = () => {
         </div>
       </header>
       <div className="main">
-        {user ? (
-          <Fragment>
-            <div className="user" onClick={logout}>
-              {user.username} 🚪
-            </div>
-            <TaskForm />
-
-            <div className="filter">
-              <button onClick={() => setHideCompleted(!hideCompleted)}>
-                {hideCompleted ? 'Show All' : 'Hide Completed'}
-              </button>
-            </div>
-
-            <ul className="tasks">
-              {tasks.map(task => (
-                <Task
-                  key={task._id}
-                  task={task}
-                  onCheckboxClick={handleToggleChecked}
-                  onDeleteClick={handleDelete}
-                />
-              ))}
-            </ul>
-          </Fragment>
-        ) : (
-          <LoginForm />
-        )}
+        <TaskForm />
+        <div className="filter">
+          <button onClick={() => setHideCompleted(!hideCompleted)}>
+            {hideCompleted ? 'Show All' : 'Hide Completed'}
+          </button>
+        </div>
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={handleToggleChecked}
+              onDeleteClick={handleDelete}
+            />
+          ))}
+        </ul>
       </div>
     </div>
   );
